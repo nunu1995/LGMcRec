d911465042c1379bf9431569b9ec2240120e4229
nunu1995 <100558538+nunu1995@users.noreply.github.com>
2024-11-20 15:18:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ class Config:
         self.seed = 42
         self.reproducibility = True
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.embedding_size = 1536
+        self.embedding_size = 64
         self.n_layers = 2
-        self.reg_weight = 0.1
-        self.kd_weight = 0.1
+        self.reg_weight = 1e-1
+        self.kd_weight = 1e-1
         self.temperature = 0.2
         self.epochs = 100
         self.metrics = ['Precision', 'Recall', 'NDCG']
