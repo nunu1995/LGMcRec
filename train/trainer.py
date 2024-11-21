41788f651a3ca5a11feafacffeabbd592be5119b
nunu1995 <100558538+nunu1995@users.noreply.github.com>
2024-11-21 14:44:31 +0800

    Update trainer.py

--- train/trainer.py
+++ train/trainer.py
@@ -4,6 +4,17 @@ from model.mclightgcn import MCLightGCN
 
 
 def initialize_trainer(config, co_matrix, user_item_criterion_ratings):
+      """
+    Initialize the trainer for the MCLightGCN model.
+
+    Args:
+        config (Config): Configuration parameters.
+        co_matrix (array): Co-occurrence matrix for criteria.
+        user_item_criterion_ratings (dict): User-item-criterion ratings.
+
+    Returns:
+        tuple: Trainer object, training data, and test data.
+    """
 
     dataset = create_dataset(config)
     train_data, valid_data, test_data = data_preparation(config, dataset)
@@ -22,6 +33,17 @@ def initialize_trainer(config, co_matrix, user_item_criterion_ratings):
 
 
 def train_and_evaluate(trainer, train_data, test_data):
+     """
+    Train the model and evaluate it on test data.
+
+    Args:
+        trainer (Trainer): RecBole trainer object.
+        train_data (Dataset): Training dataset.
+        test_data (Dataset): Testing dataset.
+
+    Returns:
+        dict: Evaluation results.
+    """
 
     best_valid_score, best_valid_result = trainer.fit(train_data)
     test_result = trainer.evaluate(test_data)
